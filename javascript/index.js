f5a1fd4b61fecb7bff6874f76a3ce76d92d6ef6e
renan <brunaduarte2307@gmail.com>
2025-10-30 22:12:27 -0300

    finalizando variavel

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,17 @@
 console.log("cheetos")
+
+//criando variavel 
+//declarando variavel e atribuindo o 102
+
+//const mediaDeNotas // CamelCase
+//const MediaNotas // PascalCase
+//const media_notas // Snack_Case
+
+let numero = 102
+console.log(numero)
+numero = 99
+console.log(numero)
+
+const nome = "Bruna"
+console.log(nome)
+//nome = "cheetos" - nao pode atribuir valor para constante depois de declarar
